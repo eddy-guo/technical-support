3c4819228a4c69a39a4c5d78e30039760b0e9dec
Eddy Guo <eddyyanruguo@gmail.com>
2021-06-25 03:32:13 -0400

    gmail security test

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let mailOptions = {
 
 transporter.sendMail(mailOptions, function (err, data) {
   if (err) {
-    console.log("Error Occurs");
+    console.log("Error Occurs", err);
   } else {
     console.log("Email sent!");
   }
